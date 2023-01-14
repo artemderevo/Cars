9d2158b0c33682b728ea2fc31000a1098ecce721
q2q2q8 <art-kash@mail.ru>
2023-01-14 20:23:27 +0300

    2

--- Cars.java
+++ Cars.java
@@ -6,6 +6,16 @@ public class Car {
     int Year;
     String Country;
 
+    Car(String Brand, String Model, double EngineVolume, String Color, int Year, String Country) {
+        this.Brand = Brand;
+        this.Model = Model;
+        this.EngineVolume = EngineVolume;
+        this.Color = Color;
+        this.Year = Year;
+        this.Country = Country;
+
+    }
+
     void info() {
         System.out.println("Марка автомобиля: " + Brand);
         System.out.println("Модель автомобиля: " + Model);
--- Main.java
+++ Main.java
@@ -1,49 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car lada = new Car();
-        lada.Brand = "Lada";
-        lada.Model = "Granta";
-        lada.EngineVolume = 1.7;
-        lada.Color = "Желтый";
-        lada.Year = 2015;
-        lada.Country = "Россия";
+        Car lada = new Car("Lada","Granta",1.7,"Желтый",2015,"Россия");
         lada.info();
 
-        Car audi = new Car();
-        audi.Brand = "Audi";
-        audi.Model = "A8 50 L TDI quattro";
-        audi.EngineVolume = 3.0;
-        audi.Color = "Черный";
-        audi.Year = 2020;
-        audi.Country = "Германия";
+        Car audi = new Car("Audi","A8 50 L TDI quattro",3.0,"Черный",2020,"Германия");
         audi.info();
 
-        Car bmw = new Car();
-        bmw.Brand = "BMW";
-        bmw.Model = "Z8";
-        bmw.EngineVolume = 3.0;
-        bmw.Color = "Черный";
-        bmw.Year = 2021;
-        bmw.Country = "Германия";
+        Car bmw = new Car("BMW","Z8",3.0,"Черный",2021,"Германия");
         bmw.info();
 
-        Car kia = new Car();
-        kia.Brand = "Kia";
-        kia.Model = "Sportage 4-го поколения";
-        kia.EngineVolume = 2.4;
-        kia.Color = "Красный";
-        kia.Year = 2018;
-        kia.Country = "Южная Корея";
+        Car kia = new Car("Kia","Sportage 4-го поколения",2.4,"Красный",2018,"Южная Корея");
         kia.info();
 
-        Car hyundai = new Car();
-        hyundai.Brand = "Hyundai";
-        hyundai.Model = "Avante";
-        hyundai.EngineVolume = 1.6;
-        hyundai.Color = "Оранжевый";
-        hyundai.Year = 2016;
-        hyundai.Country = "Южная Корея";
+        Car hyundai = new Car("Hyundai","Avante",1.6,"Оранжевый",2016,"Южная Корея");
         hyundai.info();
     }
 }
